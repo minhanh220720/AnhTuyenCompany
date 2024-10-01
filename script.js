7ade88d2bf1332692686d4c17835d92b99a0aedb
minhanh220720 <anhtnm0@gmail.com>
2024-10-01 12:29:30 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contactBtn').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Cảm ơn bạn đã gửi tin nhắn! Chúng tôi sẽ liên hệ với bạn sớm nhất.');
+    this.reset();
+});
